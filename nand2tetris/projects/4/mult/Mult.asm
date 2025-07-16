b8376d11e7cc051d32fec70ca7f8e4ad4df68c91
taiking0905 <taiki.noguchi0905@gmail.com>
2025-07-16 17:56:42 +0900

    multできた

--- nand2tetris/projects/4/mult/Mult.asm
+++ nand2tetris/projects/4/mult/Mult.asm
@@ -8,3 +8,37 @@
 // The algorithm is based on repetitive addition.
 
 //// Replace this comment with your code.
+
+// R2 = R0 * R1 を計算する
+// 結果は R2 に入る
+
+@R2
+M = 0       // R2 = 0
+@R3
+M = 0       // R3 = 0
+
+(LOOP)
+    @R3
+    D = M
+    @R1
+    D = M - D     // D = R1 - R3
+    @END
+    D;JLE         // if R3 >= R1, jump to END
+
+    @R0
+    D = M         // D = R0
+
+    @R2
+    M = M + D     // R2 += R0
+
+    @R3
+    M = M + 1     // R3++
+
+    @LOOP
+    0;JMP
+
+(END)
+    @END
+    0;JMP         // 無限ループで終了（停止の代用）
+
+
