4a63f75c78453f0f8cdcf4616bc4e3e679c00905
taiking0905 <taiki.noguchi0905@gmail.com>
2025-07-17 19:25:48 +0900

    filわかんない

--- nand2tetris/projects/4/fill/Fill.asm
+++ nand2tetris/projects/4/fill/Fill.asm
@@ -9,3 +9,42 @@
 // the screen should be cleared.
 
 //// Replace this comment with your code.
+
+@SCREEN
+D = A
+@R0
+M = D
+
+@R1 
+M = 0
+
+(LOOP)
+    @R1
+    D = M
+    //@8192
+    @2
+    D = D -A
+    @END
+    D;JGE
+
+    @KBD
+    D = M
+    @-1
+    D;JGT
+    @0
+
+    @R0
+    A = M
+    M = D
+
+    @R0 
+    M = M + 1
+
+    @R1
+    M = M + 1
+    @LOOP
+    0;JMP
+
+(END)
+@END
+0;JMP
